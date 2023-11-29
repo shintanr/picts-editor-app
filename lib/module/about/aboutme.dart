8281813dd52918410e49fd9afc543755f2368b08
shintanr <shintanurrohmah143@gmail.com>
2023-11-29 12:49:57 +0700

    update about me

--- lib/module/about/aboutme.dart
+++ lib/module/about/aboutme.dart
@@ -1,247 +1,312 @@
+// import 'package:flutter/material.dart';
+// import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+// import 'package:url_launcher/url_launcher.dart';
+
+// class AboutMe extends StatelessWidget {
+//   const AboutMe({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: Container(
+//         width: double.infinity,
+//         decoration: const BoxDecoration(
+//           gradient: LinearGradient(
+//             colors: [Colors.white, Colors.white],
+//             begin: Alignment.bottomCenter,
+//             end: Alignment.topCenter,
+//           ),
+//         ),
+//         child: Center(
+//           child: Column(
+//             mainAxisAlignment: MainAxisAlignment.center,
+//             children: [
+//               const CircleAvatar(
+//                 radius: 70,
+//                 backgroundImage: AssetImage('assets/about.jpg'),
+//               ),
+//               const SizedBox(
+//                 height: 10,
+//               ),
+//               const Text(
+//                 'Shinta Nurrohmah',
+//                 style: TextStyle(
+//                     fontSize: 24,
+//                     fontWeight: FontWeight.w600,
+//                     letterSpacing: 2),
+//               ),
+//               const SizedBox(
+//                 height: 10,
+//               ),
+//               const Text(
+//                 'Kelompok 19',
+//                 style: TextStyle(fontSize: 14),
+//               ),
+
+//       floatingActionButton: FloatingActionButton(
+//         onPressed: () {
+//           Navigator.pop(context);
+//         },
+//         backgroundColor: Colors.amber,
+//         foregroundColor: Colors.black,
+//         child: const Text('Back'),
+//       ),
+//     );
+//   }
+// }
+//
+
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class AboutMe extends StatelessWidget {
-  const AboutMe({super.key});
+  const AboutMe({Key? key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        width: double.infinity,
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
-            colors: [Colors.white, Colors.white],
-            begin: Alignment.bottomCenter,
-            end: Alignment.topCenter,
-          ),
+        appBar: AppBar(
+          title: Text('About Me'),
         ),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const CircleAvatar(
-                radius: 70,
-                backgroundImage: AssetImage('assets/about.jpg'),
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              const Text(
-                'Shinta Nurrohmah',
-                style: TextStyle(
-                    fontSize: 24,
-                    fontWeight: FontWeight.w600,
-                    letterSpacing: 2),
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              const Text(
-                'Kelompok 19',
-                style: TextStyle(fontSize: 14),
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              Container(
-                height: 0.5,
-                color: Colors.black,
-                width: 300,
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              Container(
-                width: 300,
-                height: 30,
-                alignment: Alignment.center,
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  border: Border.all(
-                    color: Colors.black,
-                    width: 0.5,
-                  ),
-                  borderRadius: BorderRadius.circular(10),
+        body: Column(
+          children: [
+            Container(
+              width: double.infinity,
+              decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [Colors.white, Colors.white],
+                  begin: Alignment.bottomCenter,
+                  end: Alignment.topCenter,
                 ),
-                child: Row(
+              ),
+              child: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: [
+                    const CircleAvatar(
+                      radius: 70,
+                      backgroundImage: AssetImage('assets/about.jpg'),
+                    ),
                     const SizedBox(
-                      width: 5,
+                      height: 10,
+                    ),
+                    const Text(
+                      'Shinta Nurrohmah',
+                      style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.w600,
+                          letterSpacing: 2),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    const Text(
+                      'Kelompok 19',
+                      style: TextStyle(fontSize: 14),
+                    ),
+                    const SizedBox(
+                      height: 10,
                     ),
                     Container(
-                      decoration: const BoxDecoration(
-                        borderRadius: BorderRadius.all(Radius.circular(50)),
-                      ),
-                      child: Container(
-                        decoration: const BoxDecoration(
-                          borderRadius: BorderRadius.all(Radius.circular(50)),
-                        ),
-                        child: const Icon(
-                          FontAwesomeIcons.github,
-                          size: 20,
+                      height: 0.5,
+                      color: Colors.black,
+                      width: 300,
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Container(
+                      width: 300,
+                      height: 30,
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        border: Border.all(
                           color: Colors.black,
+                          width: 0.5,
                         ),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Row(
+                        children: [
+                          const SizedBox(
+                            width: 5,
+                          ),
+                          Container(
+                            decoration: const BoxDecoration(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(50)),
+                            ),
+                            child: Container(
+                              decoration: const BoxDecoration(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(50)),
+                              ),
+                              child: const Icon(
+                                FontAwesomeIcons.github,
+                                size: 20,
+                                color: Colors.black,
+                              ),
+                            ),
+                          ),
+                          const SizedBox(
+                            width: 10,
+                          ),
+                          GestureDetector(
+                            onTap: () {
+                              launchURL('https://github.com/shintanr');
+                            },
+                            child: const Text('GitHub'),
+                          )
+                        ],
                       ),
                     ),
                     const SizedBox(
-                      width: 10,
-                    ),
-                    GestureDetector(
-                      onTap: () {
-                        launchURL('https://github.com/shintanr');
-                      },
-                      child: const Text('GitHub'),
-                    )
-                  ],
-                ),
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              Container(
-                width: 300,
-                height: 30,
-                alignment: Alignment.center,
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  border: Border.all(
-                    color: Colors.black,
-                    width: 0.5,
-                  ),
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                child: Row(
-                  children: [
-                    const SizedBox(
-                      width: 5,
+                      height: 10,
                     ),
                     Container(
-                      decoration: const BoxDecoration(
-                        borderRadius: BorderRadius.all(Radius.circular(50)),
+                      width: 300,
+                      height: 30,
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        border: Border.all(
+                          color: Colors.black,
+                          width: 0.5,
+                        ),
+                        borderRadius: BorderRadius.circular(10),
                       ),
-                      child: const Icon(
-                        FontAwesomeIcons.telegram,
-                        size: 20,
-                        color: Colors.black,
+                      child: Row(
+                        children: [
+                          const SizedBox(
+                            width: 5,
+                          ),
+                          Container(
+                            decoration: const BoxDecoration(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(50)),
+                            ),
+                            child: const Icon(
+                              FontAwesomeIcons.telegram,
+                              size: 20,
+                              color: Colors.black,
+                            ),
+                          ),
+                          const SizedBox(
+                            width: 10,
+                          ),
+                          GestureDetector(
+                            onTap: () {
+                              launchURL('https://t.me/javascriptrt');
+                            },
+                            child: const Text('Telegram'),
+                          ),
+                        ],
                       ),
                     ),
                     const SizedBox(
-                      width: 10,
-                    ),
-                    GestureDetector(
-                      onTap: () {
-                        launchURL('https://t.me/javascriptrt');
-                      },
-                      child: const Text('Telegram'),
-                    ),
-                  ],
-                ),
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              Container(
-                width: 300,
-                height: 30,
-                alignment: Alignment.center,
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  border: Border.all(
-                    color: Colors.black,
-                    width: 0.5,
-                  ),
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                child: Row(
-                  children: [
-                    const SizedBox(
-                      width: 5,
+                      height: 10,
                     ),
                     Container(
-                      decoration: const BoxDecoration(
-                        borderRadius: BorderRadius.all(Radius.circular(50)),
+                      width: 300,
+                      height: 30,
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        border: Border.all(
+                          color: Colors.black,
+                          width: 0.5,
+                        ),
+                        borderRadius: BorderRadius.circular(10),
                       ),
-                      child: const Icon(
-                        FontAwesomeIcons.instagram,
-                        size: 20,
-                        color: Colors.black,
+                      child: Row(
+                        children: [
+                          const SizedBox(
+                            width: 5,
+                          ),
+                          Container(
+                            decoration: const BoxDecoration(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(50)),
+                            ),
+                            child: const Icon(
+                              FontAwesomeIcons.instagram,
+                              size: 20,
+                              color: Colors.black,
+                            ),
+                          ),
+                          const SizedBox(
+                            width: 10,
+                          ),
+                          GestureDetector(
+                            onTap: () {
+                              launchURL('https://www.instagram.com/shintanrrh');
+                            },
+                            child: const Text('Instagram'),
+                          ),
+                        ],
                       ),
                     ),
                     const SizedBox(
-                      width: 10,
-                    ),
-                    GestureDetector(
-                      onTap: () {
-                        launchURL('https://www.instagram.com/shintanrrh');
-                      },
-                      child: const Text('Instagram'),
-                    ),
-                  ],
-                ),
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              Container(
-                width: 300,
-                height: 30,
-                alignment: Alignment.center,
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  border: Border.all(
-                    color: Colors.black,
-                    width: 0.5,
-                  ),
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                child: Row(
-                  children: [
-                    const SizedBox(
-                      width: 5,
+                      height: 10,
                     ),
                     Container(
-                      decoration: const BoxDecoration(
-                        borderRadius: BorderRadius.all(Radius.circular(50)),
+                      width: 300,
+                      height: 30,
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        border: Border.all(
+                          color: Colors.black,
+                          width: 0.5,
+                        ),
+                        borderRadius: BorderRadius.circular(10),
                       ),
-                      child: const Icon(
-                        FontAwesomeIcons.linkedin,
-                        size: 20,
-                        color: Colors.black,
+                      child: Row(
+                        children: [
+                          const SizedBox(
+                            width: 5,
+                          ),
+                          Container(
+                            decoration: const BoxDecoration(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(50)),
+                            ),
+                            child: const Icon(
+                              FontAwesomeIcons.linkedin,
+                              size: 20,
+                              color: Colors.black,
+                            ),
+                          ),
+                          const SizedBox(
+                            width: 10,
+                          ),
+                          GestureDetector(
+                            onTap: () {
+                              launchURL(
+                                  'https://www.linkedin.com/in/shintanurohmah/');
+                            },
+                            child: const Text('LinkedIn'),
+                          ),
+                        ],
                       ),
-                    ),
-                    const SizedBox(
-                      width: 10,
-                    ),
-                    GestureDetector(
-                      onTap: () {
-                        launchURL(
-                            'https://www.linkedin.com/in/shintanurohmah/');
-                      },
-                      child: const Text('LinkedIn'),
                     ),
                   ],
                 ),
               ),
-            ],
-          ),
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.pop(context);
-        },
-        backgroundColor: Colors.amber,
-        foregroundColor: Colors.black,
-        child: const Text('Back'),
-      ),
-    );
+            ),
+
+            // Rest of your code...
+          ],
+        ));
   }
 }
+
+// Rest of your code...
 
 void launchURL(String url) async {
   // ignore: deprecated_member_use
